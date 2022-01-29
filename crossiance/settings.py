3f84bce65adb31d841d1acb14fa7644db7fc4e86
dinagon <65805267+dinagon@users.noreply.github.com>
2022-01-29 12:41:16 +0530

    deploy

--- crossiance/settings.py
+++ crossiance/settings.py
@@ -124,7 +124,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    '/var/www/static/',
+    '/var/www/staticfiles/',
 ]
 
 
